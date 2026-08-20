a51d38258c06a90d88617adb625c5236967f175e
SaykaArab-AC <sayka.arab.cmaug26@gmail.com>
2026-08-20 23:56:12 +0530

    Update UninitialisedVariable.java

--- LogicBuildingSessions/Assignment_02/UninitialisedVariable.java
+++ LogicBuildingSessions/Assignment_02/UninitialisedVariable.java
@@ -1,34 +1,25 @@
 import java.util.Scanner;
 
 public class UninitialisedVariable {
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    char g;
+    boolean h;
+
     public static void main(String args[]){
-        //Local Variables
-        // byte a;
-        // short b;
-        // int c;
-        // long d;
-        // float e;
-        // double f;
-        // char g;
-        // boolean h;
-        //This will give compile time error.
+	UninitialisedVariable obj=new UninitialisedVariable();
 
-        byte a=-12;
-        short b=2;
-        int c=1122112299;
-        long d=999999999999999999L;
-        float e=90.9f;
-        double f=33.16;
-        char g='s';
-        boolean h=true;
-
-        System.out.println("a = "+a);
-        System.out.println("b = "+b);
-        System.out.println("c = "+c);
-        System.out.println("d = "+d);
-        System.out.println("e = "+e);
-        System.out.println("f = "+f);
-        System.out.println("g = "+g);
-        System.out.println("h = "+h);
+        System.out.println("a = "+obj.a);
+        System.out.println("b = "+obj.b);
+        System.out.println("c = "+obj.c);
+        System.out.println("d = "+obj.d);
+        System.out.println("e = "+obj.e);
+        System.out.println("f = "+obj.f);
+        System.out.println("g = "+obj.g);
+        System.out.println("h = "+obj.h);
     }
 }
